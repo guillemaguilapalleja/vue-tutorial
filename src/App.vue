ab342da0ee0665cc9f323bcb8b1761b2056ab86a
guillemaguilapalleja <guillem.aguila,palleja@estudiantat.upc.edu>
2022-09-29 18:32:05 +0200

    step 3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="main">
+  <button v-if="!connected" class="myButtonConnect" @click="toggle">Connect</button>
+  <button v-if="connected" class="myButtonDisconnect" @click="toggle">Disonnect</button>
+  <div v-if="connected" class="main">
     <Top></Top>
     <div style="display: flex; height: 50%">
       <Left></Left>
@@ -10,7 +12,7 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import Top from './components/Top.vue';
 import Left from './components/Left.vue';
 import Right from './components/Right.vue';
@@ -18,7 +20,14 @@ import Bottom from './components/Bottom.vue';
 
 export default defineComponent({
     setup() {
-        return {};
+      let connected = ref(false)
+      function toggle(){
+        connected.value = !connected.value
+      }
+        return {
+          toggle,
+          connected
+        };
     },
     components: { Top, Left, Right, Bottom }
 })
@@ -26,8 +35,21 @@ export default defineComponent({
 
 <style scoped>
   .main {
-    border-style: solid;
     height: 600px;
+  }
+  .myButtonConnect {
+    width: 80%;
+    background-color: red;
+    color:white;
+    margin-left: 10%;
+    margin-bottom: 5%;
+  }
+  .myButtonDisconnect {
+    width: 80%;
+    background-color: blue;
+    color:white;
+    margin-left: 10%;
+    margin-bottom: 1%;
   }
 
 </style>
