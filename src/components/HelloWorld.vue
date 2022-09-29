b5061c5b166b891b0ae0f01f748548f6d79338ae
guillemaguilapalleja <guillem.aguila,palleja@estudiantat.upc.edu>
2022-09-29 17:01:20 +0200

    1st part tutorial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <button @click="onButtonClick" class="buttonStyle">Click here</button>
+    <h1>Message from parent: {{ msg }}</h1>
+    <h1>self message: {{ selfMessage }}</h1>
   </div>
 </template>
 
 <script>
-export default {
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
   name: 'HelloWorld',
   props: {
     msg: String
-  }
-}
+  },
+  setup () {
+    let selfMessage = ref ("my message");
+    function onButtonClick() {
+      console.log("button clicked");
+      selfMessage.value = "button clicked";
+    }
+    return {
+      onButtonClick,
+      selfMessage
+    }
+  },
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .buttonStyle {
+    background-color: brown;
+    color: whitesmoke;
+  }
 </style>
